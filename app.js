51282efcc613873a431461b7ccfc5cf033e4c71b
Can UYUMAZ <58149556+tombensonnn@users.noreply.github.com>
2022-02-27 00:52:22 +0300

    post module added

--- app.js
+++ app.js
@@ -1,33 +1,54 @@
-const express = require('express');
-const ejs = require('ejs');
+const express = require("express");
+const mongoose = require('mongoose');
+const ejs = require("ejs");
+const Post = require('./models/Post');
+
+
+
 const app = express();
 
 
+mongoose.connect('mongodb://localhost/clenanblog-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 // TEMPLATE ENGINE
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 
 // MIDDLEWARES
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));  // url'deki datayı okumamızı sağlar. 
+app.use(express.json()); // url'deki datayı json formatına çevirir.
 
 // ROUTES
-app.get('/', (req,res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {
+    posts
+  });
 });
 
-app.get('/index', (req,res) => {
-    res.render('index');
+// app.get("/index", (req, res) => {
+//   res.render("index");
+// });
+
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
-app.get('/about', (req,res) => {
-    res.render('about');
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
 });
 
-app.get('/add', (req,res) => {
-    res.render('add');
-});
+app.post("/posts", async (req,res) => {
+  // console.log(req.body);
+  await Post.create(req.body)
+  res.redirect('/');
+})
 
-port = 3001;
+port = 3000;
 
 app.listen(port, () => {
-    console.log(`Server opened successfully. Port: ${port}`);
+  console.log(`Server started successfully. Port: ${port}`);
 });
